3676eb05199c3182e32b596a1bf3a528154a83a3
Samtropic <127030009+Samtropic@users.noreply.github.com>
2025-01-09 11:16:09 +0100

    [1-TIMESTAMP][ANGULAR] Add createdAt column on patients list

--- angular/src/app/core/services/date-format.service.ts
+++ angular/src/app/core/services/date-format.service.ts
@@ -6,14 +6,22 @@ import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 export class DateFormatService {
   private readonly frenchFormat = 'dd MMM yy';
   private readonly usFormat = "MMM d'th' yy";
+  private readonly timeFormat = 'HH:mm:ss';
 
   constructor(@Inject(LOCALE_ID) private locale: string) {}
 
-  getDateFormat(): string {
-    if (this.locale === 'fr') {
-      return this.frenchFormat;
-    } else {
-      return this.usFormat;
+  getDatetimeFormat(withTime?: boolean): string {
+    let dtFormat = '';
+    switch (this.locale) {
+      case 'fr':
+        dtFormat = this.frenchFormat;
+        break;
+      case 'us':
+        dtFormat = this.usFormat;
+        break;
+      default:
+        dtFormat = this.frenchFormat;
     }
+    return withTime ? dtFormat + ` ${this.timeFormat}` : dtFormat;
   }
 }
--- angular/src/app/protected/professional-patients/professional-patients.component.ts
+++ angular/src/app/protected/professional-patients/professional-patients.component.ts
@@ -32,6 +32,7 @@ export class ProfessionalPatientsComponent implements OnInit {
       'birthdate',
       'email',
       'phone',
+      'createdAt',
     ];
   }
 
